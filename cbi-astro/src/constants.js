ced04300b2065855f491b8c27398180d64a5ad51
Arison13 <arisonarias@gmail.com>
2026-01-14 18:21:03 -0500

    updating service times

--- cbi-astro/src/constants.js
+++ cbi-astro/src/constants.js
@@ -6,25 +6,25 @@ const serviceTimes = [
   // FIXME: add the image for each service
   {
     name: "Servicio Del Domingo",
-    times: ["9:30", "11:30"],
+    times: ["9:30 AM", "11:30 AM"],
     day: "Domingo",
     image: "/images/services/sunday.jpg"
   },
   {
     name: "Noches de Oración",
-    times: ["7:30"],
+    times: ["7:30 PM"],
     day: "Martes-Iglesia",
     image: "/images/services/prayer.jpg"
   },
   {
     name: "Noches de Oración-Virtual",
-    times: ["7:30"],
+    times: ["7:30 PM"],
     day: "Martes-Facebook Live",
     image: "/images/services/prayer.jpg"
   },
   {
     name: "Servicio de Jovenes",
-    times: ["7:30"],
+    times: ["7:30 PM"],
     day: "Viernes",
     image: "/images/services/youth.jpg"
   }
