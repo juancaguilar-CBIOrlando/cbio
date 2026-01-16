7440f56bd62356cca1afb0f3c22b2546d2967fcc
Arison13 <arisonarias@gmail.com>
2026-01-15 19:02:39 -0500

    adding basic missions data

---
+++ cbi-astro/src/helpers/missionsData.js
@@ -0,0 +1,24 @@
+import encuentrosImg from "../assets/encuentros.jpeg"
+import parejasImg from "../assets/parejas.jpeg"
+import mujeresImg from "../assets/mujeres.jpg"
+import kidsHeroImg from "../assets/kidsHero.jpg"
+import riverasImg from "../assets/riveras.jpg"
+import bautizosImg from "../assets/bautizos3.jpeg"
+
+/**
+ * Temporary placeholder content to match the reference "Misiones" screen.
+ * Swap these titles/images once we have real missions + ministry content.
+ */
+
+export const missionsSlides = [
+  { id: "misiones-1", title: "Misiones 1", image: encuentrosImg },
+  { id: "misiones-2", title: "Misiones 2", image: mujeresImg },
+  { id: "misiones-3", title: "Misiones 3", image: parejasImg },
+]
+
+export const exSlides = [
+  { id: "ex-1", title: "Ministerio EX 1", image: kidsHeroImg },
+  { id: "ex-2", title: "Ministerio EX 2", image: riverasImg },
+  { id: "ex-3", title: "Ministerio EX 3", image: bautizosImg },
+]
+
